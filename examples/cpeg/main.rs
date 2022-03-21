57b87758c212d582f09da0f893ef6474e2e07700
Sage Mitchell <dev@mx00s.com>
2022-03-21 11:18:15 -0700

    WIP: internal interface

---
+++ examples/cpeg/main.rs
@@ -0,0 +1,159 @@
+// Eventually most of this will be replaced by macros, but for now it's useful
+// to iterate on the internal macro-free interface.
+
+use std::ops::Index;
+
+use anyhow::Result;
+use once_cell::sync::Lazy;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum NonTerminal {
+    Prod,
+    Val,
+}
+
+impl peggy::NonTerminals for NonTerminal {}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Letter {
+    L0,
+    L1,
+    L2,
+    L3,
+    L4,
+    L5,
+    L6,
+    L7,
+    L8,
+    L9,
+    LMul,
+}
+
+impl peggy::Alphabet for Letter {}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Label {
+    Prod,
+    Int,
+}
+
+impl peggy::LabelSymbols for Label {}
+
+// Val ^* {× Val #Prod}
+static PROD: Lazy<peggy::Expression<Core>> = Lazy::new(|| {
+    peggy::Expression::FoldCapture(
+        Box::new(peggy::Expression::from_non_terminal(NonTerminal::Val)),
+        (
+            Box::new(peggy::Expression::Sequence(
+                Box::new(peggy::Expression::from_terminal(Letter::LMul.into())),
+                Box::new(peggy::Expression::from_non_terminal(NonTerminal::Val)),
+            )),
+            Label::Prod,
+        ),
+    )
+});
+
+// {[0123456789] #Int}
+static VAL: Lazy<peggy::Expression<Core>> = Lazy::new(|| {
+    peggy::Expression::Capture(
+        Box::new(peggy::Expression::OrderedChoice(
+            Box::new(peggy::Expression::from_terminal(Letter::L0)),
+            Box::new(peggy::Expression::OrderedChoice(
+                Box::new(peggy::Expression::from_terminal(Letter::L1)),
+                Box::new(peggy::Expression::OrderedChoice(
+                    Box::new(peggy::Expression::from_terminal(Letter::L2)),
+                    Box::new(peggy::Expression::OrderedChoice(
+                        Box::new(peggy::Expression::from_terminal(Letter::L3)),
+                        Box::new(peggy::Expression::OrderedChoice(
+                            Box::new(peggy::Expression::from_terminal(Letter::L4)),
+                            Box::new(peggy::Expression::OrderedChoice(
+                                Box::new(peggy::Expression::from_terminal(Letter::L5)),
+                                Box::new(peggy::Expression::OrderedChoice(
+                                    Box::new(peggy::Expression::from_terminal(Letter::L6)),
+                                    Box::new(peggy::Expression::OrderedChoice(
+                                        Box::new(peggy::Expression::from_terminal(Letter::L7)),
+                                        Box::new(peggy::Expression::OrderedChoice(
+                                            Box::new(peggy::Expression::from_terminal(Letter::L8)),
+                                            Box::new(peggy::Expression::from_terminal(Letter::L9)),
+                                        )),
+                                    )),
+                                )),
+                            )),
+                        )),
+                    )),
+                )),
+            )),
+        )),
+        Label::Int,
+    )
+});
+
+#[derive(Clone, Debug)]
+struct Core;
+
+impl peggy::CoreTypes for Core {
+    type NonTerminal = NonTerminal;
+    type Alphabet = Letter;
+    type Label = Label;
+}
+
+struct Grammar;
+
+impl Index<NonTerminal> for Grammar {
+    type Output = peggy::Expression<Core>;
+
+    fn index(&self, index: NonTerminal) -> &Self::Output {
+        match index {
+            NonTerminal::Prod => &PROD,
+            NonTerminal::Val => &VAL,
+        }
+    }
+}
+
+impl peggy::Grammar<Core> for Grammar {
+    fn parse(
+        &self,
+        expr: &peggy::Expression<Core>,
+        input: &[Letter],
+    ) -> Result<peggy::Derivation, peggy::Error> {
+        todo!()
+    }
+}
+
+fn main() -> Result<()> {
+    use peggy::Grammar;
+
+    const INPUT: &str = "123×45×6";
+
+    let grammar = Grammar;
+
+    let start_expr = peggy::Expression::from_non_terminal(NonTerminal::Prod);
+
+    // naive letter lexer
+    let input = INPUT
+        .chars()
+        .map(|l| match l {
+            '0' => Letter::L0,
+            '1' => Letter::L1,
+            '2' => Letter::L2,
+            '3' => Letter::L3,
+            '4' => Letter::L4,
+            '5' => Letter::L5,
+            '6' => Letter::L6,
+            '7' => Letter::L7,
+            '8' => Letter::L8,
+            '9' => Letter::L9,
+            '×' => Letter::LMul,
+            _ => todo!("handle invalid input"),
+        })
+        .collect::<Vec<_>>();
+
+    let derivation = grammar.parse(&start_expr, &input)?;
+
+    // TODO: ensure `derivation` has the same information found in `derivation.txt`
+
+    // TODO: ensure `derivation` encodes a concrete syntax tree that can
+    //   precisely reconstruct `INPUT`.
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,97 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::ops::Index;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {}
+
+// alphabet's letters must be unique (and, in case of ranges, non-overlapping)
+pub trait Alphabet: Clone {}
+
+// non_terminals must be unique
+pub trait NonTerminals: Clone {}
+
+// labels must be unique
+pub trait LabelSymbols: Clone {}
+
+pub trait CoreTypes: Clone {
+    type NonTerminal: NonTerminals;
+    type Alphabet: Alphabet;
+    type Label: LabelSymbols;
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Expression<T>
+where
+    T: CoreTypes,
+{
+    // ε
+    Empty,
+    // a
+    Terminal(T::Alphabet),
+    // A
+    NonTerminal(T::NonTerminal),
+    // e1 e2
+    Sequence(Box<Self>, Box<Self>),
+    // e1 / e2
+    OrderedChoice(Box<Self>, Box<Self>),
+    // e*
+    ZeroOrMore(Box<Self>),
+    // !e
+    Not(Box<Self>),
+    // {e #L}
+    Capture(Box<Self>, T::Label),
+    // e1 ^* {e2 #L}
+    FoldCapture(Box<Self>, (Box<Self>, T::Label)),
+}
+
+impl<T> Expression<T>
+where
+    T: CoreTypes,
+{
+    pub fn from_non_terminal(non_terminal: T::NonTerminal) -> Self {
+        Self::NonTerminal(non_terminal)
     }
+
+    pub fn from_terminal(terminal: T::Alphabet) -> Self {
+        Self::Terminal(terminal)
+    }
+
+    // e+ ==> e e*
+    pub fn one_or_more(&self) -> Self {
+        Self::Sequence(
+            Box::new((*self).clone()),
+            Box::new(Self::ZeroOrMore(Box::new((*self).clone()))),
+        )
+    }
+
+    // [abc] ==> 'a' / 'b' / 'c'
+    pub fn any_of(exprs: &[Self]) -> Option<Self> {
+        exprs.iter().rfold(None, |acc, head| {
+            Some(acc.map_or_else(
+                || head.clone(),
+                |tail| Self::OrderedChoice(Box::new(head.clone()), Box::new(tail)),
+            ))
+        })
+    }
+
+    // e? ==> e / ε
+    pub fn optional(&self) -> Self {
+        Self::OrderedChoice(Box::new(self.clone()), Box::new(Self::Empty))
+    }
+
+    // &e ==> !(!e)
+    pub fn and(&self) -> Self {
+        Self::Not(Box::new(Self::Not(Box::new(self.clone()))))
+    }
+}
+
+// TODO: elaborate
+pub struct Derivation;
+
+pub trait Grammar<T>: Index<T::NonTerminal, Output = Expression<T>>
+where
+    T: CoreTypes,
+{
+    fn parse(&self, expr: &Expression<T>, input: &[T::Alphabet]) -> Result<Derivation, Error>;
 }
